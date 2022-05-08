e7d9710fbedf078994bbecda9a0f6e5704d798a4
SimonKrafft <simonkrafft@gmx.net>
2022-05-08 22:33:41 +0200

    initialize registration widget

---
+++ seg_utils/ui/registration_display.py
@@ -0,0 +1,70 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+from seg_utils.ui.image_viewer import ImageViewer
+from seg_utils.ui.annotation_group import AnnotationGroup
+
+
+class CenterDisplayWidget(QWidget):
+    """ widget to manage the central display in the GUI
+    controls a QGraphicsView and a QGraphicsScene for drawing on top of a pixmap """
+
+    sRequestLabelListUpdate = pyqtSignal(int)
+    CREATE, EDIT = 0, 1
+
+    def __init__(self, *args):
+        super(CenterDisplayWidget, self).__init__(*args)
+
+        # main components of the display
+        self.scene = QGraphicsScene()
+        self.image_viewer = ImageViewer(self.scene)
+
+        self.pixmap = QGraphicsPixmapItem()
+        self.scene.addItem(self.pixmap)
+        self.annotations = AnnotationGroup()
+        self.scene.addItem(self.annotations)
+
+        # put the viewer in the ImageDisplay-Frame
+        self.image_viewer.setFrameShape(QFrame.NoFrame)
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.image_viewer)
+
+    def mousePressEvent(self, event: QMouseEvent):
+        # TODO: add the drawing mode control. right now this will create a new shape on every click
+        self.annotations.create_shape()
+        event.accept()
+
+    def clear(self):
+        """This function deletes all currently stored labels
+        and triggers the image_viewer to display a default image"""
+        self.scene.b_isInitialized = False
+        self.image_viewer.b_isEmpty = True
+        self.scene.clear()
+        self.set_labels([])
+
+    def get_pixmap_dimensions(self):
+        return [self.pixmap.pixmap().width(), self.pixmap.pixmap().height()]
+
+    def init_image(self, pixmap: QPixmap, labels):
+        self.pixmap.setPixmap(pixmap)
+        # self.set_labels(labels)
+
+    def is_empty(self):
+        return self.image_viewer.b_isEmpty
+
+    def set_initialized(self):
+        self.scene.b_isInitialized = True
+        self.image_viewer.b_isEmpty = False
+
+    def set_mode(self, mode: int):
+        assert mode in [self.CREATE, self.EDIT]
+        self.scene.mode = mode
+
+
+if __name__ == '__main__':
+    test_vid = r"C:\Users\Somers\Desktop\GRK008.MP4"
+    app = QApplication(["test"])
+    vid = CenterDisplayWidget()
+    vid.show()
+    app.exec_()
